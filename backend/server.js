3ac97eed11a2bbabde73fa08bf7134b393b1e843
Yogeshk76 <yogeshkumar60508@gmail.com>
2025-03-28 20:49:20 +0530

    backend folder structure

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+import express from 'express';
+import cors from 'cors';
+import {connectDB} from './config/db.js';
+
+
+const app = express();
+app.use(cors());
+
+
+const port = process.env.PORT || 3000;
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+}
+);
+
+
+
+app.listen(port, () => {
+  connectDB();
+  console.log('Server is running on port 3000');
+}
+);
+
